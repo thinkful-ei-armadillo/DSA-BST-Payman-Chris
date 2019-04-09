af5b4595777c56e1bc31a4519006695fe126b4ac
Payman Kossari <Paymankossari@gmail.com>
2019-04-09 15:47:45 -0400

    is same finished

--- drills.js
+++ drills.js
@@ -11,16 +11,7 @@ const BinarySearchTree = require('./bst.js');
 
 function main() {
   //drill 2
-  const BST = new BinarySearchTree();
-  //3,1,4,6,9,2,5,7
-  BST.insert(3, '3');
-  BST.insert(1, '1');
-  BST.insert(4, '4');
-  BST.insert(6, '6');
-  BST.insert(9, '9');
-  BST.insert(2, '2');
-  BST.insert(5, '5');
-  BST.insert(7, '7');
+
   //console.log(BST.right);
 
   const BST2 = new BinarySearchTree();
@@ -42,11 +33,11 @@ function main() {
 
   console.log(tree(BST2));
 
-  console.log(heightOfTree(BST));
+  //console.log(heightOfTree(BST));
 
   console.log(isBST(BST2));
 
-  console.log(thirdLargest(BST));
+  //console.log(thirdLargest(BST));
 
   const BST3 = new BinarySearchTree();
   BST3.insert(899);
@@ -63,6 +54,24 @@ function main() {
   BST3.insert(900, '');
 
   console.log('test', thirdLargest(BST3));
+
+  /* 4
+    /    \
+   2      6
+ /  \    /  \
+1    3  5    7 */
+
+  const BST = new BinarySearchTree();
+  //3,1,4,6,9,2,5,7
+  BST.insert(4, '9');
+  BST.insert(2, '2');
+  //BST.insert(6, '5');
+  BST.insert(1, '7');
+  BST.insert(3, '3');
+  //BST.insert(5, '1');
+  //BST.insert(7, '4');
+
+  console.log(isBalanced(BST));
 }
 
 //drill 3 - this recursive alogrithm will print out the values of the tree in order
@@ -102,8 +111,6 @@ function isBST(tree) {
 }
 
 function thirdLargest(tree) {
-  debugger;
-
   const largest = findLargest(tree);
   let secondLarg = null;
   if (largest.left) {
@@ -132,4 +139,58 @@ function findLargest(tree) {
   }
 }
 
+function isBalanced(tree) {
+  debugger;
+  const min = findMinHeight(tree);
+  const height = heightOfTree(tree);
+  if (height - min > 1) return false;
+  return true;
+}
+
+function findMinHeight(tree) {
+  if (tree === null || tree.key === null) {
+    return 0;
+  } else {
+    return 1 + Math.min(findMinHeight(tree.left), findMinHeight(tree.right));
+  }
+}
+
+function isSame(arr1, arr2) {
+  if (arr1.length === 0 && arr2.length === 0) {
+    return true;
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  if (arr1[0] !== arr2[0]) {
+    return false;
+  }
+  if (arr1.length === 1 && arr2.length === 1) {
+    return true;
+  }
+  let middle = arr1[0];
+
+  let largestArr1 = [];
+  let smallestArr1 = [];
+  let largestArr2 = [];
+  let smallestArr2 = [];
+  for (let i = 1; i < arr1.length; i++) {
+    if (arr1[i] > middle) {
+      largestArr1.push(arr1[i]);
+    } else {
+      smallestArr1.push(arr1[i]);
+    }
+    if (arr2[i] > middle) {
+      largestArr2.push(arr2[i]);
+    } else {
+      smallestArr2.push(arr2[i]);
+    }
+  }
+
+  return isSame(largestArr1, largestArr2) && isSame(smallestArr1, smallestArr2);
+}
+
 main();
+
+console.log(isSame([3, 5, 4, 6, 1, 0, 2], [3, 1, 5, 2, 4, 6, 0]));
